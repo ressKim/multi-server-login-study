c36d14ed6b1ca8f06c541e05abc8deebb740b2b6
ressKim <sh21dlat@gmail.com>
2021-08-03 18:42:51 +0900

    BaseTimeEntity 추가 및 UserEntity, LoginSession 에 적용

--- src/main/java/com/study/multiserverlogin/user/UserService.java
+++ src/main/java/com/study/multiserverlogin/user/UserService.java
@@ -1,21 +1,15 @@
 package com.study.multiserverlogin.user;
 
-import com.study.multiserverlogin.domain.session.LoginSessionRepository;
 import com.study.multiserverlogin.domain.user.UserEntity;
 import com.study.multiserverlogin.domain.user.UserEntityRepository;
-import com.study.multiserverlogin.response.LoginResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserEntityRepository userEntityRepository;
-    private final LoginSessionRepository loginSessionRepository;
 
     public Boolean userSave(UserValue userValue) {
         //아이디 중복이면 false return
